4d89f6d9653c4dafc1495564ed0ccb6aa36d94b6
Blake Walsh <blakeofthepath@gmail.com>
2016-01-04 21:22:07 +0200

    Made update_templates not try to push

--- update_templates.sh
+++ update_templates.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-cd pootle/translations
-git push
-
 ssh pootle@vps.suttacentral.net '\
     /usr/bin/ionice -c 2 -n 7 -p$$ &&\
     source "/home/pootle/.pyenv/versions/pootle/bin/activate" &&\
