8f23762d37c3606bcb4757d713b43295f0cfab65
Blake Walsh <blakeofthepath@gmail.com>
2016-02-04 22:59:06 +0200

    New version of Pali Lookup using IndexedDB backend

--- pootle/static/js/suttacentral.js
+++ pootle/static/js/suttacentral.js
@@ -2,6 +2,85 @@
 import $ from 'jquery';
 import _ from 'underscore';
 
+
+/* Based on code from Digital Pali Reader */
+/* Made slightly less nutty */
+var comparePaliAlphabet = (function(){
+
+var reorder = [
+'#','0','1','2','3','4','5','6','7','8','9',
+'a','ā','i','ī','u','ū','e','o','ṃ','k','kh','g','gh','ṅ','c','ch','j',
+'jh','ñ','ṭ','ṭh','ḍ','ḍh','ṇ','t','th','d','dh','n','p','ph','b','bh',
+'m','y','r','l','ḷ','v','s','h'
+]
+
+var oldorder = [
+'#','0','1','2','3','4','5','6','7','8','9',
+'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+'S','T','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+'q','r','s','t','u','v','w','x','y','z'
+]
+
+var neworder = []
+var roo = '';
+for(var w = 0; w < reorder.length; w++) {
+    roo = reorder[w];
+    neworder[roo] = oldorder[w];
+}
+
+function sortaz(mydata){  // sort pali array
+
+    mydata = mydata.sort(comparePaliAlphabet);
+    for (i in mydata) {
+        mydata[i] = mydata[i].replace(/^.*###/,''); // remove sorted words, return the rest
+    }
+    return mydata;
+}
+
+function sortStrip(word) {
+    word = word.replace(/ṁ/g, 'ṃ');
+    word = word.replace(/Ṁ/g, 'Ṃ');
+    word = word.toLowerCase().replace(/[^a-zāīūṃṅñṭḍṇḷ#]/g,'');
+    return word;
+}
+
+return function comparePaliAlphabet(a,b) {
+
+    if (a.length == 0 || b.length == 0) return;
+
+    let two = [sortStrip(a),sortStrip(b)];
+    let nwo = [];
+    for (let i = 0; i < two.length; i++) {
+        let wordval = '';
+        for (let c = 0; c < two[i].length; c++) {
+
+            let onechar = two[i].charAt(c);
+            if(onechar == '#') break;
+
+            let twochar = onechar + two[i].charAt(c+1);
+
+            if (neworder[twochar]) {
+                wordval+=neworder[twochar];
+                c++;
+            }
+            else if (neworder[onechar]) {
+                wordval+=neworder[onechar];
+            }
+            else {
+                wordval+=onechar;
+            }
+        }
+        two[i] = wordval;
+        nwo[i] = wordval;
+    }
+    nwo.sort();
+    //dalert(a+' '+nwo+' '+b+' ' + two+' '+(nwo[0] == two[0]));
+    if(nwo[0] != two[0]) return 1;
+    else return -1;
+
+}
+})();
+
 function debugmsg(msg){
     console.log('SuttaCentral: ' + msg);
 }
@@ -13,57 +92,52 @@ window.suttacentral = {
         const self = window.suttacentral,
             sidebar = $('.translate-left');
         sidebar.find('#tm').remove();
-        
-        var dev = false;
-        
-        if ($('.translation-text[lang=pi]').length) {
-            if (!self.paliLookupFetchPromise) {
-                if (dev) {
-                    $(document.body).append('\
-                    <script src="http://localhost/js/vendor/elasticsearch-5.0.0.js"></script>\
-                    <script src="http://localhost/js/vendor/underscore-1.8.3.js"></script>\
-                    <script src="http://localhost/js/lib/idb-cache.js"></script>\
-                    <script src="http://localhost/js/lib/text-nodes.js"></script>\
-                    <script src="http://localhost/js/lib/pali-sort.js"></script>\
-                    <script src="http://localhost/js/lib/sorted-stringify.js"></script>\
-                    <script src="http://localhost/js/sc_popup.js"></script>\
-                    <script src="http://localhost/js/paliLookup2.0.js"></script>\
-                    <link rel="stylesheet" href="http://localhost/css/compiled/paliLookup2.2-standalone.css">');
-                    self.paliLookupFetchPromise = $.Deferred();
-                    _.delay(function(){self.paliLookupFetchPromise.resolve()}, 200);
-                } else {
-                    self.paliLookupFetchPromise = $.ajax({
-                        url: "/assets/js/paliLookup2.3-standalone.js",
-                        dataType: "script",
-                        cache: false
-                    });
-                }
-                
-            }
-            
 
-            self.paliLookupFetchPromise.then(suttacentral.activatePaliLookup)
-                                       .then(suttacentral.activateGlossary);
+        let dev = false;
+
+        if ($('.translation-text[lang=pi]').length) {
+          let settings = {
+            selector: '.lookup',
+            main: 'body',
+            lookupWorkerSrc: '/assets/js/lookup/lookup-worker-1.0.js',
+            fromLang: 'pi',
+            toLang: 'en',
+            dataFile: '/assets/js/lookup/pi2en-entries-1.0.json',
+            glossaryFile: '/assets/js/lookup/pi2en-glossary-1.0.json'
+          }
+          $.ajax('/assets/js/lookup/lookup-1.0.js').then( () => {
+            let lookupUtility = this.activatePaliLookup(settings);
+            lookupUtility.ready.then( () => {
+              this.activateGlossary(lookupUtility);
+              $('body').on('click', '.view-row, input', function(e){
+                if ($(e.currentTarget).parents('.text-popup')) {
+                  return
+                }
+                lookupUtility.popupClass.removeAllNow();
+              })
+            })
+          })
+
         }
-        
+
         /* Shortcuts */
-        var currentIndex = parseInt($('#item-number').val(), 10),
+        let currentIndex = parseInt($('#item-number').val(), 10),
             maxIndex = parseInt($('#items-count').text(), 10);
-        
+
         /*
         // Create a version of gotoIndex which can only be calld once
         // otherwise extreme problems occur if the keyboard shortcut is
         // invoked more than once.
         var gotoIndex = _.once(_.bind(PTL.editor.gotoIndex, PTL.editor));
-        
+
         shortcut.add('ctrl+shift+up', function(e) {
             gotoIndex(Math.max(1, currentIndex - 10));
         });
-        
+
         shortcut.add('ctrl+shift+down', function(e) {
             gotoIndex(Math.min(currentIndex + 10, maxIndex));
         });
-        
+
         shortcut.add('shift+pageup', function(e) {
             gotoIndex(1);
         });
@@ -73,25 +147,16 @@ window.suttacentral = {
         */
         // Rebind ctrl+shift+n
         shortcut.add('ctrl+shift+g', shortcut.all_shortcuts["ctrl+shift+n"].callback);
-        
+
         // Copy Original
-        
+
         shortcut.add('alt+down', function() {
           $('.js-copyoriginal').click()
         });
         shortcut.add('ctrl+b', function() {
           $('.js-copyoriginal').click()
         });
-    
-    },
-    getScripts: function(scripts) {
-        var promises = [];
-        
-        scripts.forEach(function(script) {
-            promises.push(jQuery.getScript(script));
-        })
-        
-        return jQuery.when.apply(promises);
+
     },
     /* Automatically convert ascii punctuation to unicode */
     unicodeify: function(e) {
@@ -107,49 +172,53 @@ window.suttacentral = {
             element.caret(caret);
         }
     },
-    activatePaliLookup: function() {        
-        suttacentral.exports = {elasticsearch_api_url: 'http://localhost:9200/'};
-        suttacentral.paliLookup.targets = '.translate-focus .translation-text[lang=pi]';
-        suttacentral.paliLookup.mouseovertarget = 'body';
-        suttacentral.paliLookup.main = 'body'
-        
-        suttacentral.paliLookup.markupGenerator.excludeFn = function(node) {
-            if ($(node.parentNode).is('[class^=highlight]')) return true
-            return false
-        }
-        suttacentral.paliLookup.activate();
-        suttacentral.popup.clear()
+    activatePaliLookup: function(settings) {
+
+
+      this.lookupUtility = new LookupUtility(settings);
+
+      this.markupGenerator = new MarkupGenerator();
+      console.log(this.markupGenerator);
+
+      // this.markupGenerator.shouldExclude = function(node) {
+      //   if ($(node.parentNode).is('[class^=highlight]')) return true
+      //   return false
+      // }
+
+      this.markupGenerator.startMarkupOnDemand({
+          targetSelector: '.translate-focus .translation-text[lang=pi]',
+          exclusions: '[class^=highlight]'});
+      return this.lookupUtility;
     },
-    activateGlossary: function() {
+    activateGlossary: function(lookupUtility) {
         var tmPanel = $('<div id="tm" class="sidebar" dir="ltr">\
             <div class="sidetitle" lang="en-gb">Terminology:</div>\
         </div>');
-            
+
         var tmUnitTmpl = $('<div class="tm-unit js-editor-copytext" title="Insert the translated term into the editor">\
           <span class="tm-original" dir="ltr" lang="pi"></span>\
           <span class="tm-translation" dir="ltr" lang="en"></span>\
         </div>');
-        
+
         var text = $('.translate-focus .translation-text').text();
-        if (!suttacentral.paliLookup.glossary.client) {
-            return
-        }
-        suttacentral.paliLookup.glossary.getEntries(text).then(function(resp) {
-            var hits = resp.hits.hits;
-            hits.sort(function(a, b) {return comparePaliAlphabet(a._source.term, b._source.term)});
-            hits.forEach(function(hit) {
+        let words = new Set(text.split(this.lookupUtility.markupGenerator.splitRex));
+        words.delete('');
+        lookupUtility.glossary.getEntries([...words]).then(function(results) {
+
+            results.sort(function(a, b) {return comparePaliAlphabet(a.term, b.term)});
+            results.forEach(function(hit) {
                 var tmUnit = tmUnitTmpl.clone();
-                var term = hit._source.term;
-                if (hit._source.context) {
-                    term += '<span style="font-weight: normal"> (' + hit._source.context + ')</span>';
+                var term = hit.term;
+                if (hit.context) {
+                    term += '<span style="font-weight: normal"> (' + hit.context + ')</span>';
                 }
                 tmUnit.find('.tm-original').html(term);
-                tmUnit.find('.tm-translation').text(hit._source.gloss);
-                tmUnit.data('translation-aid', hit._source.gloss + ' ');
+                tmUnit.find('.tm-translation').text(hit.gloss);
+                tmUnit.data('translation-aid', hit.gloss + ' ');
                 tmPanel.append(tmUnit);
             })
             if (tmPanel.children().length > 1) {
-                $('.sidebar').append(tmPanel);
+                $('.translate-left').append(tmPanel);
             }
         })
     },
@@ -173,8 +242,6 @@ window.suttacentral = {
     },
 }
 
-$(document).on('change keyup', 'textarea.translation', suttacentral.unicodeify);  
+$(document).on('change keyup', 'textarea.translation', suttacentral.unicodeify);
 
 $(document).on('editor_ready', suttacentral.init);
-
-
