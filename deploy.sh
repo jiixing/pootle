78424691e9759c1755eaafe56c02d0564b8b59d7
Blake Walsh <blakeofthepath@gmail.com>
2016-02-08 23:26:39 +0200

    Added fix to deploy.sh - update packages before migrate

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ ssh pootle@vps.suttacentral.net '\
     source "/home/pootle/.pyenv/versions/pootle/bin/activate" &&\
     cd /home/pootle/pootle &&\
     git pull &&\
+    pip install -r requirements/dev.txt &&\
     ./manage.py migrate &&\
     make assets &&\
     sudo supervisorctl restart pootle\
